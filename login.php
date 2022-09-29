1a86ccdd98a7cd72415dd0f87741c3b3d86216d4
Ambaza-Marcelo <marcelin@net-telecom.net>
2022-09-29 21:12:23 +0200

    verify exist username

--- login.php
+++ login.php
@@ -3,7 +3,30 @@
 	include('connection.php');
 	//affichage des donneees dans le tableau 
 
-			$data = $conn->query("select * from messages")->fetchAll();
+	if (isset($_POST['submit'])) {
+
+		$username = htmlspecialchars($_POST['username']);
+		$password = md5(htmlspecialchars($_POST['password']));
+
+		$data = $conn->query("select * from users")->fetchAll();
+
+		if (!empty($username) && !empty($password)) {
+			$count = 0;
+			foreach ($data as $row) {
+			if ($row['username'] == $username && $row['password'] == $password) {
+				header('Location: admin.php')
+			}else{
+				$count = $count + 1;
+			}
+		}
+		if ($count > 0) {
+			$message = "mot de passe ou nom utilisateur incorect";
+		}
+		}else{
+			echo "veuillez saisir tous les entrees";
+		}
+		
+	}
 
 
 ?>
@@ -16,7 +39,10 @@
 	<link rel="stylesheet" type="text/css" href="css/styles.css">
 </head>
 <body style="margin-top: 12%;margin-left: 30%;">
-	<div class="container">
+	<div class="container"> 
+		<?php if (isset($message)) {
+			echo $message;
+		} ;?>
 		<form action="login.php" method="POST">
 		<div class="row">
 			<div class="col-md-6">
@@ -31,7 +57,7 @@
 			</div>
 		</div>
 		<div>
-			<button class="btn btn-success">&nbsp;&nbsp;Connexion</button>
+			<button class="btn btn-success" name="submit">&nbsp;&nbsp;Connexion</button>
 		</div>
 		<div>
 			Si vous n'avez pas le compte cliquer <a href="register.php">Ici</a>
